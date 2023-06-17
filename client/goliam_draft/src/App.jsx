f258fe286301bba42c79eb3a2af44b6c94878715
Hamza Ait <hait74144@gmail.com>
2023-06-17 20:24:44 +0200

    Nuevo componente Oferta V1

--- client/goliam_draft/src/App.jsx
+++ client/goliam_draft/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Oferta } from "./Oferta.jsx";
+export {Oferta} from "./Oferta.jsx";
 function App() {
   return (
+  
     <div className="App">
-      <h1>Goliam-draft</h1>
-      <h2>Presupueto</h2>
+      <Oferta></Oferta>
+      hola
     </div>
   );
 }
---
+++ client/goliam_draft/src/Oferta.jsx
@@ -0,0 +1,18 @@
+import "./Oferta.css";
+
+
+
+export function Oferta(){
+    return(
+        <div className="contenedoroferta">
+            <div className="contenido">
+                <p>ENVIOS A TODA ESPAÑA  📦&#x1F1EA;&#x1F1F8;</p>
+                </div>
+                <div className="xc">
+                <button class="xcerrar">X</button>
+                </div>
+                
+            
+        </div>
+    )
+}
