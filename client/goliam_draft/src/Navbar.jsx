79cd35eb1fd542668139ee5e71c25f296c176356
Hamza Ait <hait74144@gmail.com>
2023-06-20 12:55:39 +0200

    New React icons

---
+++ client/goliam_draft/src/Navbar.jsx
@@ -0,0 +1,15 @@
+export function Navbar(){
+  return(
+     <div className="navcontainer">
+      <div className="logonav">
+        <p>GOLIAM</p>
+      </div>
+      <div className="linksnav">
+        <a href="#" className="Catalogo"></a>
+      </div>
+     <div className="iconav"></div>
+     </div>
+
+  )
+
+}
--- client/goliam_draft/src/Oferta.jsx
+++ client/goliam_draft/src/Oferta.jsx
@@ -3,16 +3,25 @@ import "./Oferta.css";
 
 
 export function Oferta(){
+let contenall = document.getElementsByClassName("contenedoroferta");
+function cerrar(){
+  contenall.remove()
+  console.log("HOL")
+}
+
+
     return(
         <div className="contenedoroferta">
             <div className="contenido">
                 <p>ENVIOS A TODA ESPAÑA  📦&#x1F1EA;&#x1F1F8;</p>
                 </div>
                 <div className="xc">
-                <button class="xcerrar">X</button>
+                <button class="xcerrar" OnClick={cerrar}>X</button>
                 </div>
                 
             
         </div>
+        
+        
     )
 }
